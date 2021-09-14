eac0c41ac27ebfd1033931d8d4d5182369d0de1b
Rekkuzan5 <cstapp88@gmail.com>
2021-09-14 14:39:47 -0500

    cleaned up tsparticles on App.js and starting to add API.

--- src/App.js
+++ src/App.js
@@ -6,116 +6,114 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-
+const particleOptions = {
+  //id="tsparticles",
+  //options: {
+    background: {
+      color: {
+        value: "#202020",
+      },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "grab",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 0,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#FF0000",
+      },
+      links: {
+        color: "#FF0000",
+        distance: 250,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        //enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 3,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 40,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        random: true,
+        value: 2,
+      },
+    },
+    detectRetina: true,
+  //}
+}
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.particlesInit = this.particlesInit.bind(this);
-    this.particlesLoaded = this.particlesLoaded.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
   }
 
-  particlesInit(main) {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  onInputChange = (event) => {
+    console.log(event.target.value);
   }
 
-  particlesLoaded(container) {
-    console.log(container);
+  onButtonSubmit = () => {
+    console.log('click');
   }
 
   render() {
     return (
       <div className="App">
         <Particles  className='particles'
-            id="tsparticles"
-            init={this.particlesInit}
-            loaded={this.particlesLoaded}
-            options={{
-              background: {
-                color: {
-                  value: "#202020",
-                },
-              },
-              fpsLimit: 60,
-              interactivity: {
-                detectsOn: "canvas",
-                events: {
-                  onClick: {
-                    enable: true,
-                    mode: "push",
-                  },
-                  onHover: {
-                    enable: true,
-                    mode: "grab",
-                  },
-                  resize: true,
-                },
-                modes: {
-                  bubble: {
-                    distance: 400,
-                    duration: 2,
-                    opacity: 0.8,
-                    size: 40,
-                  },
-                  push: {
-                    quantity: 0,
-                  },
-                  repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                  },
-                },
-              },
-              particles: {
-                color: {
-                  value: "#FF0000",
-                },
-                links: {
-                  color: "#FF0000",
-                  distance: 250,
-                  enable: true,
-                  opacity: 0.5,
-                  width: 1,
-                },
-                collisions: {
-                  //enable: true,
-                },
-                move: {
-                  direction: "none",
-                  enable: true,
-                  outMode: "bounce",
-                  random: false,
-                  speed: 3,
-                  straight: false,
-                },
-                number: {
-                  density: {
-                    enable: true,
-                    value_area: 800,
-                  },
-                  value: 30,
-                },
-                opacity: {
-                  value: 0.5,
-                },
-                shape: {
-                  type: "circle",
-                },
-                size: {
-                  random: true,
-                  value: 2,
-                },
-              },
-              detectRetina: true,
-            }}
+            params={particleOptions}
           />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
           {/*<FaceRecognition />*/}
       </div>
       );
